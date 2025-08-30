49ede9a5ca0d814fa9d0307d03adfde519bb1b92
Monupatel62 <monuvlogs54@gmail.com>
2025-08-30 17:38:43 +0000

    add initial data types demonstration in JavaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+//alert("Hello, World!"); //we are using node.js, not browser
+
+console.log(3+
+    3) // code readability should be high
+console.log("Hello, World!");
+let name = "monu patel"
+let age = 21
+let isLoggedIn = false
+let state;
+
+
+//number =>2 to power 53
+//bigint => arbitrary precision
+//string => sequence of characters
+//boolean => true/false
+//null => standalone value
+//undefined => no value assigned
+//symbol => unique and immutable value
+//object => collection of key-value pairs
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof 9007199254741991n); //bigint
+console.log(typeof Symbol("id"));
+console.log(typeof undefined)
